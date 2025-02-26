604fc2d9c7856ca98f45f96890484fd79e832db5
Sazol Sarker <sazolsarker1@gmail.com>
2025-02-26 15:03:15 +0600

    Post api receives data from client and saves into MongoDB database's userCollection

--- index.js
+++ index.js
@@ -5,13 +5,61 @@ const port=process.env.PORT||5000
 
 // MIDDLEWARE
 app.use(cors())
+app.use(express.json())
+
+// MONGODB
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://sazolsarker1:hspun2ClsZYUYx6V@cluster0.uomr8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tls=true&tlsAllowInvalidCertificates=true";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // CREATE DATABASE
+    const database = client.db("usersDB");
+    const userCollection = database.collection("userCollection");
+
+    // ALL client-server-DB APIs goes here
+    app.post('/user',async(req,res)=>{
+      const user=req.body;
+      console.log("Server got user data=>",user);
+      // send user to mongodb
+      const result = await userCollection.insertOne(user);
+
+      // console.log(result);
+      res.send(result)
+    })
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
 
 // Requests
 app.get('/',(req,res)=>{
-    res.send('Welcome to server hooomepage')
+    res.send('Welcome to server homepage!')
 })
 
 app.listen(port,()=>{
